b8349b8b9c107a788bb7b21b005d4e472f0d40dc
satanas66 <ODFenergia37GIT>
2023-01-20 14:09:34 +0100

    Implementación del servicio requerido para la devolución de un Precio aplicandole los parámetros dada en la request (fecha de aplicación, id del procuto, id de la cadena)
    En esta clase se implementan métodos privados para el tratamiento de las fechas

--- src/main/java/es/plataforma/core/product/service/PriceService.java
+++ src/main/java/es/plataforma/core/product/service/PriceService.java
@@ -1,10 +1,13 @@
 package es.plataforma.core.product.service;
 
+import es.plataforma.core.product.dto.RequestPriceDto;
 import es.plataforma.core.product.entity.Price;
 
 import java.util.List;
 
 public interface PriceService {
 
-    public List<Price> findAllPrice();
+    List<Price> findAllPrice();
+
+    Price findPriceWithHigherPriority(RequestPriceDto requestPriceDto);
 }
--- src/main/java/es/plataforma/core/product/service/PriceServiceImpl.java
+++ src/main/java/es/plataforma/core/product/service/PriceServiceImpl.java
@@ -1,10 +1,14 @@
 package es.plataforma.core.product.service;
 
+import es.plataforma.core.product.dto.RequestPriceDto;
 import es.plataforma.core.product.entity.Price;
 import es.plataforma.core.product.repository.PriceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.List;
 
 @Service
@@ -16,5 +20,43 @@ public class PriceServiceImpl implements PriceService{
     @Override
     public List<Price> findAllPrice() {
         return priceRepository.findAll();
+    }
+
+    @Override
+    public Price findPriceWithHigherPriority(RequestPriceDto requestPriceDto) {
+        LocalDateTime startApplicationDate = generateStartApplicationDate(requestPriceDto.getApplicationDate());
+        LocalDateTime endApplicationDate = generateEndApplicationDate(requestPriceDto.getApplicationDate());
+        return priceRepository.getPriceByApplicationDateProductIdAndCadenaId(startApplicationDate, endApplicationDate, requestPriceDto.getProductId(), requestPriceDto.getCadenaId());
+    }
+
+    /**
+     * Método para generar la fecha y hora de inicio de un día
+     * @param date
+     * @return LocalDateTime
+     */
+    private LocalDateTime generateStartApplicationDate(String date){
+        LocalDateTime ldt = converterStringToLocalDateTime(date);
+        return LocalDateTime.of(ldt.getYear(), ldt.getMonth(), ldt.getDayOfMonth(), 00, 00, 00);
+    }
+
+    /**
+     * Método para generar la fecha y hora de final de un día
+     * @param date
+     * @return LocalDateTime
+     */
+    private LocalDateTime generateEndApplicationDate(String date){
+        LocalDateTime ldt = converterStringToLocalDateTime(date);
+        return LocalDateTime.of(ldt.getYear(), ldt.getMonth(), ldt.getDayOfMonth(), 23, 59, 59);
+    }
+
+    /**
+     * Método encargado de convertir un String a LocalDateTime
+     * @param date
+     * @return LocalDateTime
+     */
+    private LocalDateTime converterStringToLocalDateTime(String date){
+        DateTimeFormatter formateador = new DateTimeFormatterBuilder()
+                .parseCaseInsensitive().append(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).toFormatter();
+        return LocalDateTime.parse(date, formateador);
     }
 }
