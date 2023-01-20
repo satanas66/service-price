99cb5f28360d1bd983d2f5acc24c59cb6ce8bd39
satanas66 <ODFenergia37GIT>
2023-01-20 14:03:38 +0100

    Creación de método getPriceByApplicationDateProductIdAndCadenaId en la interfaz reposirotio e instancia de la query que cumpla con el documento de requitos de esta prueba técnica

--- src/main/java/es/plataforma/core/product/repository/PriceRepository.java
+++ src/main/java/es/plataforma/core/product/repository/PriceRepository.java
@@ -3,10 +3,16 @@ package es.plataforma.core.product.repository;
 import es.plataforma.core.product.entity.Cadena;
 import es.plataforma.core.product.entity.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PriceRepository extends JpaRepository<Price, Integer> {
 
-    public List<Price> findByCadena(Cadena cadena);
+    List<Price> findByCadena(Cadena cadena);
+
+    @Query(value = "select * from tbl_price where start_date between cast(:startDate AS timestamp) and cast(:endDate AS timestamp) and product_id= :productId and brand_id= :brandId order by priority desc limit 1", nativeQuery = true)
+    Price getPriceByApplicationDateProductIdAndCadenaId(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("productId") Integer productId, @Param("brandId") Integer brandId);
 }
