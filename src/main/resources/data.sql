979982bb4e9637f581e7f38c9f09fd508cafa25b
satanas66 <ODFenergia37GIT>
2023-01-20 14:00:47 +0100

    Cambios en configuración del proyecto, se ordena el versionado en el pom.xml, se cambia el nombre de la base de datos H2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ CREATE TABLE tbl_price (id INTEGER PRIMARY KEY, start_date TIMESTAMP, end_date T
 
 INSERT INTO tbl_cadena (id, name) VALUES (1, 'ZARA');
 
-INSERT INTO tbl_price (id, start_date, end_date, product_id, priority, price, curr, brand_id) VALUES (1, '2020-06-14 00:00:00', '2020-12-31 23:59:59', 1, 35455, 35.50, 'EUR', 1);
-INSERT INTO tbl_price (id, start_date, end_date, product_id, priority, price, curr, brand_id) VALUES (2, '2020-06-14 15:00:00', '2020-06-14 18:30:00', 1, 35455, 25.45, 'EUR', 1);
-INSERT INTO tbl_price (id, start_date, end_date, product_id, priority, price, curr, brand_id) VALUES (3, '2020-06-15 00:00:00', '2020-06-15 11:00:00', 1, 35455, 30.50, 'EUR', 1);
-INSERT INTO tbl_price (id, start_date, end_date, product_id, priority, price, curr, brand_id) VALUES (4, '2020-06-15 16:00:00', '2020-12-31 23:59:59', 1, 35455, 38.95, 'EUR', 1);
+INSERT INTO tbl_price (id, start_date, end_date, priority, product_id, price, curr, brand_id) VALUES (1, '2020-06-14 00:00:00', '2020-12-31 23:59:59', 0, 35455, 35.50, 'EUR', 1);
+INSERT INTO tbl_price (id, start_date, end_date, priority, product_id, price, curr, brand_id) VALUES (2, '2020-06-14 15:00:00', '2020-06-14 18:30:00', 1, 35455, 25.45, 'EUR', 1);
+INSERT INTO tbl_price (id, start_date, end_date, priority, product_id, price, curr, brand_id) VALUES (3, '2020-06-15 00:00:00', '2020-06-15 11:00:00', 1, 35455, 30.50, 'EUR', 1);
+INSERT INTO tbl_price (id, start_date, end_date, priority, product_id, price, curr, brand_id) VALUES (4, '2020-06-15 16:00:00', '2020-12-31 23:59:59', 1, 35455, 38.95, 'EUR', 1);
